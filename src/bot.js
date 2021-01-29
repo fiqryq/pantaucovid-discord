0c3ad9917dccfa7279aec12d5a2c0ab72b4164bc
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-01-29 10:17:39 +0700

    add cmd ,date format , num format.

--- src/bot.js
+++ src/bot.js
@@ -4,35 +4,63 @@ require('dotenv').config();
 const {Client} = require('discord.js');
 const client = new Client();
 const axios = require('axios');
+const moment = require('moment');
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
 // Command
 const PREFIX = "/";
 
-var positif = null;
 let url = 'https://covid19.mathdro.id/api/countries/ID';
-
-async function getUser() {
-    try {
-      const response = await axios.get(url);
-      this.positif = response.data.confirmed.value;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-getUser();
 
 client.on('ready', () =>{
     console.log(`${client.user.tag} has logged in`);
 })
 
-client.on('message',(message) => {
-   if(message.author.bot) return;
-   if(message.content.startsWith(PREFIX)){
-       const CMD_NAME = message.content.substring(PREFIX.length);
-       if (CMD_NAME === 'info'){
-           message.reply('kasus : ', positif)
-       }
-   }
-})
+// Convert to number format
+function numberFormat(number){
+    return number.toLocaleString("id-ID");
+}
+
+// convert date format using moment
+function convertStrDateToPattern(strDate , pattern, local){
+    return moment(strDate).locale(local).format(pattern);
+}
+
+// what the fuck is this? i still code on response axioss wew.
+async function getResponse() {
+    try {
+      const response = await axios.get(url);
+      
+        client.on('message',(message) => {
+            if(message.author.bot) return;
+            if(message.content.startsWith(PREFIX)){
+                const CMD_NAME = message.content.substring(PREFIX.length);
+                if (CMD_NAME === 'info'){
+                    message.reply(`jumlah kasus`)
+                }
+                if (CMD_NAME === 'positif'){
+                    message.channel.send('Total kasus positif Indonesia : ' + numberFormat(response.data.confirmed.value))
+                }
+                if (CMD_NAME === 'sembuh'){
+                    message.channel.send('Total kasus sembuh Indonesia : ' + numberFormat(response.data.recovered.value))
+
+                }
+                if(CMD_NAME === 'meninggal'){
+                    message.channel.send('Total kasus meninggal Indonesia : ' + numberFormat(response.data.deaths.value))
+                }
+                if(CMD_NAME === 'kasus'){
+                    message.channel
+                    .send('Update kasus corona di indonesia : \n'
+                    + convertStrDateToPattern(response.data.lastUpdate,'LLLL ','id-ID') 
+                    + numberFormat(response.data.confirmed.value))
+                }
+            } 
+        })
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+getResponse();
+
