9cb9f0e95715ab91dd04e5cb0564c86ed9d58cc4
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-01-29 10:58:07 +0700

    add info

--- src/bot.js
+++ src/bot.js
@@ -8,9 +8,11 @@ const moment = require('moment');
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
 // Command
-const PREFIX = "/";
+const PREFIX = "#";
 
 let url = 'https://covid19.mathdro.id/api/countries/ID';
+let github = 'https://github.com/fiqryq';
+let avatar = 'freepik';
 
 client.on('ready', () =>{
     console.log(`${client.user.tag} has logged in`);
@@ -36,23 +38,39 @@ async function getResponse() {
             if(message.content.startsWith(PREFIX)){
                 const CMD_NAME = message.content.substring(PREFIX.length);
                 if (CMD_NAME === 'info'){
-                    message.reply(`jumlah kasus`)
+                    message.channel
+                    .send('## Bot Info ##\n'
+                    + 'Basic Command : \n' 
+                    + '#info : untuk melihat info bot \n'
+                    + '#positif : untuk melihat total kasus positif di indonesia \n'
+                    + '#sembuh : untuk melihat total kasus sembuh di indonesia \n' 
+                    + '#meninggal : untuk melihat total kasus meninggal di indonesia \n'
+                    + '#kasus : untuk melihat total kasus corona indonesia \n'
+                    + '--------------------------------------------------------------- \n' 
+                    + 'Devloper bot : Fiqry choerudin \n'
+                    + 'github : ' + github + '\n'
+                    + 'avatar : ' + avatar + '\n'
+                    + 'Data covid : ' + url);
                 }
                 if (CMD_NAME === 'positif'){
-                    message.channel.send('Total kasus positif Indonesia : ' + numberFormat(response.data.confirmed.value))
+                    message.channel.send('🟡 Total kasus positif Indonesia : ' 
+                    + numberFormat(response.data.confirmed.value))
                 }
                 if (CMD_NAME === 'sembuh'){
-                    message.channel.send('Total kasus sembuh Indonesia : ' + numberFormat(response.data.recovered.value))
-
+                    message.channel.send('🟢 Total kasus sembuh Indonesia : ' 
+                    + numberFormat(response.data.recovered.value))
                 }
                 if(CMD_NAME === 'meninggal'){
-                    message.channel.send('Total kasus meninggal Indonesia : ' + numberFormat(response.data.deaths.value))
+                    message.channel.send('🔴 Total kasus meninggal Indonesia : ' 
+                    + numberFormat(response.data.deaths.value))
                 }
                 if(CMD_NAME === 'kasus'){
                     message.channel
-                    .send('Update kasus corona di indonesia : \n'
-                    + convertStrDateToPattern(response.data.lastUpdate,'LLLL ','id-ID') 
-                    + numberFormat(response.data.confirmed.value))
+                    .send('⏱️ Update Kasus '
+                    + convertStrDateToPattern(response.data.lastUpdate,'LLLL ','id-ID') + '\n'
+                    + 'Total kasus positif Indonesia   : ' + numberFormat(response.data.confirmed.value) + '\n'
+                    + 'Total kasus sembuh Indonesia    : ' + numberFormat(response.data.recovered.value) + '\n'
+                    + 'Total kasus meninggal Indonesia : ' + numberFormat(response.data.deaths.value))
                 }
             } 
         })
